aab7eb51745ed45d804955d9c494f99045dfb512
Radu Groza <radu.groza@bitstone.eu>
2017-01-19 13:08:20 +0200

    Removed the deprecated field value shortcode

--- leads-additional-fields.php
+++ leads-additional-fields.php
@@ -19,103 +19,87 @@ Docs here:- http://thrivethemes.com/?post_type=tkb_item&p=61122
 /**
  * Filter Example 1 - Attach to every form in TCB and Leads
  *
- * @param $lead_group - (WP Post Object) Lead Group content type
- * @param $form_type - (WP Post Object) Form Type content type
+ * @param $lead_group     - (WP Post Object) Lead Group content type
+ * @param $form_type      - (WP Post Object) Form Type content type
  * @param $form_variation - (Array) Form variation data
+ *
  * @return string - additional dynamic code to appear before </form> tag
  */
-function leads_additional_fields_ex1($additional_content, $lead_group, $form_type, $form_variation)
-{
-    return $additional_content . "<input type='hidden' name='additional_leads_field_ex1' value='success'>";
+function leads_additional_fields_ex1( $additional_content, $lead_group, $form_type, $form_variation ) {
+	return $additional_content . "<input type='hidden' name='additional_leads_field_ex1' value='success'>";
 }
 
-add_filter("tve_additional_fields", "leads_additional_fields_ex1", 10, 4);
+add_filter( "tve_additional_fields", "leads_additional_fields_ex1", 10, 4 );
 
 
 /**
  * Filter Example 2 - Only apply logic to a lead group with a particular name
  *
  * @param $additional_content - Might be empty or string added bug other filters before
- * @param $lead_group - (WP Post Object) Lead Group content type
- * @param $form_type - (WP Post Object) Form Type content type
- * @param $form_variation - (Array) Form variation data
+ * @param $lead_group         - (WP Post Object) Lead Group content type
+ * @param $form_type          - (WP Post Object) Form Type content type
+ * @param $form_variation     - (Array) Form variation data
+ *
  * @return string - additional dynamic code to appear before </form> tag
  */
-function leads_additional_fields_ex2($additional_content, $lead_group, $form_type, $form_variation)
-{
+function leads_additional_fields_ex2( $additional_content, $lead_group, $form_type, $form_variation ) {
 
-    if(empty($lead_group)) {
-        return $additional_content;
-    }
+	if ( empty( $lead_group ) ) {
+		return $additional_content;
+	}
 
-    $my_group_name = 'My Group 1';
+	$my_group_name = 'My Group 1';
 
-    if(is_object($lead_group) && $lead_group->post_title === $my_group_name) {
-        return $additional_content . "<input type='hidden' name='additional_leads_field_ex2' value='success'>";
-    }
+	if ( is_object( $lead_group ) && $lead_group->post_title === $my_group_name ) {
+		return $additional_content . '<input type="hidden" name="additional_leads_field_ex2" value="success">';
+	}
 
-    return $additional_content;
+	return $additional_content;
 }
 
-add_filter("tve_additional_fields", "leads_additional_fields_ex2", 10, 4);
+add_filter( 'tve_additional_fields', 'leads_additional_fields_ex2', 10, 4 );
 
 
 /**
  * Filter Example 3 - Only apply logic to a lead group with a particular name and an individual form type
  *
  * @param $additional_content - Might be empty or string added bug other filters before
- * @param $lead_group - (WP Post Object) Lead Group content type
- * @param $form_type - (WP Post Object) Form Type content type
- * @param $form_variation - (Array) Form variation data
+ * @param $lead_group         - (WP Post Object) Lead Group content type
+ * @param $form_type          - (WP Post Object) Form Type content type
+ * @param $form_variation     - (Array) Form variation data
+ *
  * @return string - additional dynamic code to appear before </form> tag
  */
-function leads_additional_fields_ex3($additional_content, $lead_group, $form_type, $form_variation)
-{
-    if(empty($lead_group) || empty($lead_group)) {
-        return $additional_content;
-    }
+function leads_additional_fields_ex3( $additional_content, $lead_group, $form_type, $form_variation ) {
+	if ( empty( $lead_group ) || empty( $lead_group ) ) {
+		return $additional_content;
+	}
 
-    $my_group_name = 'My Group 1';
-    $particular_form_type = 'Ribbon';
+	$my_group_name        = 'My Group 1';
+	$particular_form_type = 'Ribbon';
 //    $particular_form_type = 'Lightbox';
 //    $particular_form_type = 'Widget';
 //    $particular_form_type = 'Post Footer';
 //    $particular_form_type = 'Slide in';
 //    $particular_form_type = 'In content';
 
-    if(is_object($lead_group) && $lead_group->post_title === $my_group_name && is_object($form_type) && $form_type->post_title === $particular_form_type) {
-        return $additional_content . "<input type='hidden' name='additional_leads_field_ex3' value='success'>";
-    }
+	if ( is_object( $lead_group ) && $lead_group->post_title === $my_group_name && is_object( $form_type ) && $form_type->post_title === $particular_form_type ) {
+		return $additional_content . '<input type="hidden" name="additional_leads_field_ex3" value="success">';
+	}
 
-    return $additional_content;
+	return $additional_content;
 }
 
-add_filter("tve_additional_fields", "leads_additional_fields_ex3", 10, 4);
-
-
-/**
- * Shortcode example 1 - return a field value
- *
- * Use this shortcode to return a dynamic value
- * Useage: [tve_value_shortcode]
- */
-function leads_value()
-{
-    return 'Input field value';
-}
-
-add_shortcode('tve_value_shortcode', 'leads_value');
-
+add_filter( "tve_additional_fields", "leads_additional_fields_ex3", 10, 4 );
 
 /**
- * Shortcode example 2 - return entire hidden input
+ * Shortcode example 1 - return entire hidden input
  *
  * Use this shortcode to return a dynamic code (perhaps additional fields) or tracking pixesl
  * Useage: [tve_fields_shortcode]
  */
-function leads_fields()
-{
-    return '<input type="hidden" name="additional_leads_field" value="success">';
+function leads_fields() {
+	return '<input type="hidden" name="additional_leads_field" value="success">';
 }
 
-add_shortcode('tve_fields_shortcode', 'leads_fields');
+add_shortcode( 'tve_fields_shortcode', 'leads_fields' );
